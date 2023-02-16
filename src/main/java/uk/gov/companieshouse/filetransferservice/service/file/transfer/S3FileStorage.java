ab67ad3c7da027c576b24c7d35fb775d046875bd
adeharrison-ch <aharrison1@companieshouse.gov.uk>
2023-02-16 15:27:33 +0000

    Added storage interface and S3 implementation

---
+++ src/main/java/uk/gov/companieshouse/filetransferservice/service/file/transfer/S3FileStorage.java
@@ -0,0 +1,53 @@
+package uk.gov.companieshouse.filetransferservice.service.file.transfer;
+
+import uk.gov.companieshouse.api.model.filetransfer.FileApi;
+import uk.gov.companieshouse.api.model.filetransfer.FileDetailsApi;
+
+import java.util.Optional;
+
+/**
+ * An implementation of the FileStorageStrategy for S3
+ */
+public class S3FileStorage implements FileStorageStrategy {
+    /**
+     * Upload a file to S3
+     *
+     * @param file to upload
+     * @return id used in subsequent calls on the S3 file resource
+     */
+    @Override
+    public String save(FileApi file) {
+        return null;
+    }
+
+    /**
+     * Download a file from S3
+     *
+     * @param id of the file to retrieve
+     * @return Empty, if there is no such file, otherwise the File wrapped in an optional
+     */
+    @Override
+    public Optional<FileApi> load(String id) {
+        return Optional.empty();
+    }
+
+    /**
+     * Retrieve a file's details from S3
+     *
+     * @param id of the file details to retrieve
+     * @return Empty, if there is no such file, otherwise the File wrapped in an optional
+     */
+    @Override
+    public Optional<FileDetailsApi> getFileDetails(String id) {
+        return Optional.empty();
+    }
+
+    /**
+     * Deletes the file from S3
+     *
+     * @param id of the file to delete
+     */
+    @Override
+    public void delete(String id) {
+    }
+}
