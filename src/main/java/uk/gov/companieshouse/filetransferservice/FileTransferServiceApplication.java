318e257de77126c8d7f9b29d98e49eaaef8c5a75
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2023-02-17 08:17:20 +0000

    Added main file

---
+++ src/main/java/uk/gov/companieshouse/filetransferservice/FileTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.filetransferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileTransferServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileTransferServiceApplication.class, args);
+    }
+}
+
