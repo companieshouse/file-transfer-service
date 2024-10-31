a1b7eacd40feb0fb9addd722c1f6bf1d1a8ebfc5
vaibhavcompanieshouse <vkumar@companieshouse.gov.uk>
2024-10-31 10:20:20 +0000

    Added naming changes for file-transfer

--- terraform/groups/ecs-service/s3.tf
+++ terraform/groups/ecs-service/s3.tf
@@ -21,13 +21,13 @@ data "aws_s3_bucket" "file_transfer_bucket_secure" {
 
 
 resource "aws_kms_grant" "file_transfer_encryption_key_grant" {
-  name              = "${var.stage}-file-transfer-api-key-grant"
+  name              = "${var.stage}-file-transfer-service-key-grant"
   key_id            = aws_kms_key.file_transfer_encryption_key.key_id
   grantee_principal = aws_iam_role.file_transfer_service_execution.arn
   operations        = ["Encrypt", "Decrypt", "GenerateDataKey"]
 }
 
 resource "aws_iam_role" "file_transfer_service_execution" {
-  name               = "${var.stage}-file-transfer-api-ecs-execution"
+  name               = "${var.stage}-file-transfer-service-ecs-execution"
   assume_role_policy = data.aws_iam_policy_document.file_transfer_service_trust.json
 }
