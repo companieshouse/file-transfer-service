3ea7f380a73fe801e17b741bbd2694882e1a82f7
vaibhavcompanieshouse <vkumar@companieshouse.gov.uk>
2024-11-01 11:46:12 +0000

    added health check test case

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -44,12 +44,12 @@ module "ecs-service" {
   enable_listener         = true
 
   # Load balancer configuration
+  use_task_container_healthcheck    = true
   lb_listener_arn                = data.aws_lb_listener.service_lb_listener.arn
   lb_listener_rule_priority      = local.lb_listener_rule_priority
   lb_listener_paths              = local.lb_listener_paths
   healthcheck_path               = local.healthcheck_path
   healthcheck_matcher            = local.healthcheck_matcher
-  use_task_container_healthcheck = true
 
   # Docker container details
   docker_registry   = var.docker_registry
