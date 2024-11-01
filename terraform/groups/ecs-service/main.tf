40e1dd82e689bb1a10f053413741dfdee64b8676
vaibhavcompanieshouse <vkumar@companieshouse.gov.uk>
2024-11-01 15:44:55 +0000

    added health check ignore

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -44,7 +44,7 @@ module "ecs-service" {
   enable_listener         = true
 
   # Load balancer configuration
-  use_task_container_healthcheck    = true
+  use_task_container_healthcheck = true
   lb_listener_arn                = data.aws_lb_listener.service_lb_listener.arn
   lb_listener_rule_priority      = local.lb_listener_rule_priority
   lb_listener_paths              = local.lb_listener_paths
