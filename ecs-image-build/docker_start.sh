d4648fae320095a1aeacb2e339d9cbd0a8046939
vaibhavcompanieshouse <vkumar@companieshouse.gov.uk>
2024-11-18 12:31:11 +0000

    added changes for heap size

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -3,5 +3,5 @@
 # Start script for extensions-api
 
 PORT=8080
-ENV JAVA_OPTS="-Xmx2g -verbose:gc -XX:+UseG1GC"
-exec java $JAVA_OPTS -jar -Dserver.port="${PORT}" "file-transfer-service.jar"
+JAVA_OPTS="-Xmx2g -verbose:gc -XX:+UseG1GC"
+exec java ${JAVA_OPTS} -jar -Dserver.port="${PORT}" "file-transfer-service.jar"
