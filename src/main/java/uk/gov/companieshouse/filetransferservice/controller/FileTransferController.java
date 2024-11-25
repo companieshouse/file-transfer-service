b527818f1c344281e2a786ad398d193ed3387a75
vaibhavcompanieshouse <vkumar@companieshouse.gov.uk>
2024-11-25 13:20:33 +0000

    removed for pr

--- src/main/java/uk/gov/companieshouse/filetransferservice/controller/FileTransferController.java
+++ src/main/java/uk/gov/companieshouse/filetransferservice/controller/FileTransferController.java
@@ -233,9 +233,4 @@ public class FileTransferController {
         return fileStorageStrategy.load(fileId, fileDetails).orElseThrow(notFoundException);
     }
 
-    public static String formatSize(long v) {
-        if (v < 1024) return v + " B";
-        int z = (63 - Long.numberOfLeadingZeros(v)) / 10;
-        return String.format("%.1f %sB", (double)v / (1L << (z*10)), " KMGTPE".charAt(z));
-    }
 }
