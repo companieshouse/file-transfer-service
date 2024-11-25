e2d6dbf6ba486bf93335bac197901025300692c2
vaibhavcompanieshouse <vkumar@companieshouse.gov.uk>
2024-11-25 13:19:57 +0000

    added changes for new loadbalancer

--- src/main/java/uk/gov/companieshouse/filetransferservice/FileTransferServiceApplication.java
+++ src/main/java/uk/gov/companieshouse/filetransferservice/FileTransferServiceApplication.java
@@ -1,11 +1,7 @@
 package uk.gov.companieshouse.filetransferservice;
 
-import org.apache.commons.logging.Log;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class FileTransferServiceApplication {
