533ff77aec005b9ec2d94ea632610a9c5d7e5c73
Harish Marimuthu <hmarimuthu@companieshouse.gov.uk>
2025-01-07 10:27:51 +0000

    JU-1198 Adding only one file transfer service. Initial config.

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -3,5 +3,4 @@
 # Start script for extensions-api
 
 PORT=8080
-JAVA_OPTS="-Xmx2g -verbose:gc -XX:+UseG1GC"
-exec java ${JAVA_OPTS} -jar -Dserver.port="${PORT}" "file-transfer-service.jar"
+exec java -jar -Dserver.port="${PORT}" "file-transfer-service.jar"
