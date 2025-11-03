1a847184f0ba5e9f5a4e83d6f2da9249d832f81b
bcullerton <bcullerton@companieshouse.gov.uk>
2025-11-03 16:16:11 +0000

    Add consistent spacing between varaibles

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -34,6 +34,7 @@ variable "desired_task_count" {
   description = "The desired ECS task count for this service"
   default     = 1 # defaulted low for dev environments, override for production
 }
+
 variable "required_cpus" {
   type        = number
   description = "The required cpu resource for this service. 1024 here is 1 vCPU"
@@ -57,6 +58,7 @@ variable "max_task_count" {
   description = "The maximum number of tasks for this service."
   default     = 3
 }
+
 variable "use_fargate" {
   type        = bool
   description = "If true, sets the required capabilities for all containers in the task definition to use FARGATE, false uses EC2"
@@ -68,30 +70,28 @@ variable "use_capacity_provider" {
   description = "Whether to use a capacity provider instead of setting a launch type for the service"
   default     = true
 }
+
 variable "service_autoscale_enabled" {
   type        = bool
   description = "Whether to enable service autoscaling, including scheduled autoscaling"
   default     = true
 }
+
 variable "service_autoscale_target_value_cpu" {
   type        = number
   description = "Target CPU percentage for the ECS Service to autoscale on"
   default     = 50 # 100 disables autoscaling using CPU as a metric
 }
+
 variable "service_scaledown_schedule" {
   type        = string
   description = "The schedule to use when scaling down the number of tasks to zero."
-  # Typically used to stop all tasks in a service to save resource costs overnight.
-  # E.g. a value of '55 19 * * ? *' would be Mon-Sun 7:55pm.  An empty string indicates that no schedule should be created.
-
   default = ""
 }
+
 variable "service_scaleup_schedule" {
   type        = string
   description = "The schedule to use when scaling up the number of tasks to their normal desired level."
-  # Typically used to start all tasks in a service after it has been shutdown overnight.
-  # E.g. a value of '5 6 * * ? *' would be Mon-Sun 6:05am.  An empty string indicates that no schedule should be created.
-
   default = ""
 }
 
@@ -132,11 +132,13 @@ variable "eric_cpus" {
   description = "The required cpu resource for eric. 1024 here is 1 vCPU"
   default     = 256
 }
+
 variable "eric_memory" {
   type        = number
   description = "The required memory for eric"
   default     = 512
 }
+
 variable "eric_version" {
   type        = string
   description = "The version of the eric container to run."
